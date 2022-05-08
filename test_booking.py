4714075cc013e8ec6d20075575c1639dc516a82e
JakubGaluszka96 <jakub.galuszka@interia.pl>
2022-05-08 14:44:27 +0200

    GetBooking test

--- test_booking.py
+++ test_booking.py
@@ -1,21 +1,31 @@
+from tokenize import Number
 import pytest
 import requests
 import json
 import jsonpath
 
-def test_GetBookingIds(url="https://restful-booker.herokuapp.com/booking"):
-    #url="https://restful-booker.herokuapp.com/booking"
+def test_GetBookingIds():
+    url="https://restful-booker.herokuapp.com/booking"
     resp = requests.get(url)
-    json_resp = json.loads(resp.text)
-    bookingid = jsonpath.jsonpath(json_resp,'bookingid')
-    assert resp.status_code==200
+    json_resp = resp.json()
+    assert resp.status_code==200, "Wrong response code."
+    for i in range(0,len(json_resp)):
+        assert "bookingid" in json_resp[i] , "Data key has changed"
+    for i in range(0,len(json_resp)):
+        assert isinstance((json_resp[i]['bookingid']), int) , "At least one of booking Id's is not an intiger"
+
 
 
 url="https://restful-booker.herokuapp.com/booking"
 resp = requests.get(url)
 json_resp = resp.json()
-bookingid = jsonpath.jsonpath(json_resp,'bookingid')
-print(json_resp)
+print(json_resp[0])
+print("bookingid" in json_resp[0])
+#print(resp.headers)
+#print(resp.cookies)
+#print(resp.encoding)
+#print(resp.url)
+
 
 
 
